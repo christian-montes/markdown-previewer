16c609dad905907ce0fd3f9ad3c142ecacc05ceb
Christian Montes <christian.montes8991@gmail.com>
2021-02-24 02:46:28 -0800

    added state hook to function component

--- src/index.js
+++ src/index.js
@@ -1,18 +1,48 @@
-import React from "react";
+import React, {useState} from "react";
 import ReactDOM from "react-dom";
-// eslint-disable-next-line no-unused-vars
-import _ from "lodash"
+const marked = require('marked');
 
-class Welcome extends React.Component {
-  render() {
-    return <h1>Hello World from React boilerplate</h1>;
-  }
+// setting options for marked
+// marked takes a string of markdown to parse
+const renderer = new marked.Renderer();
+marked.setOptions({
+  gfm: true,
+  breaks: true
+})
+
+// creating a function component
+function MDPreviewer() {
+  // eslint-disable-next-line no-unused-vars
+  const [markdown, setMarkdown] = useState(parsedText)
+
+  return (
+    <>
+      Enter Github flavored markdown:
+      <br />
+      <textarea
+      // placeholder variable added after
+        id='preview'
+        onChange={(e) => {setMarkdown(marked(e.target.value, {renderer: renderer}))}}
+        defaultValue={text}>
+      </textarea>
+      <br /> <br />
+
+      Parsed markdown (HTML):
+      <br />
+      <div
+        id='parsed'
+        dangerouslySetInnerHTML={{__html: markdown}}>
+      </div>
+    </>
+  )
 }
 
+const text = `<h2>This is a level 2 header markdown format</h2>`;
+const parsedText = marked(text, {renderer: renderer})
 
 
 const containerElem = document.createElement('div');
 containerElem.setAttribute('id', 'root');
 document.body.appendChild(containerElem);
 
-ReactDOM.render(<Welcome />, document.getElementById("root"));
+ReactDOM.render(<MDPreviewer />, document.getElementById("root"));
